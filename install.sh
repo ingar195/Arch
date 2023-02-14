32955478c8ad4008f9ef078248d502697090ad03
ingar <ingar@megarden.no>
2023-02-14 19:59:54 +0000

    Wil not install with if statement

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ sudo pacman -S --noconfirm --needed base-devel git rust
 
 # TODO: Add error handing of install 
 # Install Paru helper
-if [ $(which paru) != "/usr/bin/paru" ]; then
-    git clone https://aur.archlinux.org/paru.git
-    cd paru && makepkg -si && cd ..
-    sudo rm -R paru
-fi
+#if [ $(which paru) != "/usr/bin/paru" ]; then
+git clone https://aur.archlinux.org/paru.git
+cd paru && makepkg -si && cd ..
+sudo rm -R paru
+#fi
 
 sudo sed -i 's/#BottomUp/BottomUp/g' /etc/paru.conf
 sudo sed -i 's/#SudoLoop/SudoLoop/g' /etc/paru.conf
