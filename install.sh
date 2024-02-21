59629ce3cbb0f5c8ef230b98db9a7eda294a9e5b
Ingar <ingar@megarden.no>
2024-02-21 21:10:01 +0100

    cleanup

--- install.sh
+++ install.sh
@@ -17,7 +17,13 @@ sudo sed -i 's/#SudoLoop/SudoLoop/g' /etc/paru.conf
 sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
 
 # Install Packages
-paru -S --noconfirm --needed zsh arandr alacritty remmina-plugin-rdesktop docker sshpass remmina ansible qbittorrent gnu-netcat qemu-full networkmanager-l2tp networkmanager-strongswan remmina-plugin-ultravnc screen meld betterlockscreen_rapid-git dnsmasq rclone ntfs-3g flameshot acpid bc numlockx spotify-launcher unzip usbutils dmidecode autorandr pavucontrol variety feh git tree virt-manager dunst xclip xorg-xkill rofi acpilight nautilus scrot teamviewer network-manager-applet xautolock man powertop networkmanager nm-connection-editor network-manager-applet openvpn slack-desktop wget python google-chrome freecad gparted peak-linux-headers kicad i3exit polybar parsec-bin can-utils visual-studio-code-bin ttf-nerd-fonts-symbols libreoffice-fresh gnome-keyring subversion
+paru -S --noconfirm --needed acpid alacritty ansible arandr autorandr bc betterlockscreen_rapid-git can-utils \
+    dnsmasq docker dmidecode dunst feh flameshot freecad gnu-netcat gparted google-chrome gnome-keyring \
+    i3exit kicad libreoffice-fresh man meld nautilus network-manager-applet networkmanager networkmanager-l2tp \
+    networkmanager-strongswan ntfs-3g numlockx openvpn pavucontrol peak-linux-headers polybar powertop \
+    qbittorrent qemu-full rclone remmina remmina-plugin-rdesktop remmina-plugin-ultravnc rofi scrot screen \
+    slack-desktop sshpass spotify-launcher subversion ttf-nerd-fonts-symbols unzip usbutils variety \
+    visual-studio-code-bin wget xautolock xclip xorg-xkill zsh
 
 code --install-extension alexcvzz.vscode-sqlite
 code --install-extension atlassian.atlascode
