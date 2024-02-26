f32ccd0c56bcfc35a0c47cebb7ac8794850502f6
ingar frostad <ingar@megarden.no>
2024-02-26 22:17:06 +0100

    more work on secureboot setup

--- secure_boot.sh
+++ secure_boot.sh
@@ -3,6 +3,10 @@
 paru -S --noconfirm --needed  efitools sbctl
 
 sudo chattr -i /sys/firmware/efi/efivars/{PK,KEK,db}*
+
+sudo sbctl create-keys
+sudo sbctl enroll-keys -m 
+
 sudo sbctl sign -s /boot/vmlinuz-linux
 sudo sbctl sign -s /boot/EFI/BOOT/BOOTX64.EFI
 
