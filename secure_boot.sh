dce39c7a15a539a6a926a2fd4930a2623e6ee89c
ingar <ingar@megarden.no>
2024-02-26 21:09:31 +0100

    started on secure boot

---
+++ secure_boot.sh
@@ -0,0 +1,11 @@
+
+
+paru -S --noconfirm --needed  efitools sbctl
+
+sudo chattr -i /sys/firmware/efi/efivars/{PK,KEK,db}*
+sudo sbctl sign -s /boot/vmlinuz-linux
+sudo sbctl sign -s /boot/EFI/BOOT/BOOTX64.EFI
+
+sudo sbctl sign -s -o /usr/lib/systemd/boot/efi/systemd-bootx64.efi.signed /usr/lib/systemd/boot/efi/systemd-bootx64.efi
+
+sudo bootctl install
